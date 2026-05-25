cab12e7daf7aaf890a480b805c44dccf24895bbe
LFFavoretto <favorettof14@gmail.com>
2026-05-25 09:34:25 -0300

    chore: reorganizei a estrutura da solução

---
+++ backend/SistemaDividaConsole/Models/Cliente.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaDividasConsole.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Nome é obrigatório")]
+        [StringLength(100)]
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage ="Campo CPF é obrigatório.")]
+        [StringLength(14)]
+        [RegularExpression(@"^\d{11}$", ErrorMessage ="CPF Inválido. Use apenas números.")]
+        public string Cpf { get; set; }
+
+        [Required(ErrorMessage = "Campo Data de Nascimento é obrigatório.")]
+        public DateTime DataNascimento { get; set; }
+
+        public int Idade
+        {
+            get
+            {
+                var atual = DateTime.Today;
+                var anos = atual.Year - DataNascimento.Year;
+                var diaAnoNascimento = atual.AddYears(-anos);
+
+                if (DataNascimento > diaAnoNascimento)
+                {
+                    anos--;
+                }
+
+                return anos;
+            }
+        }
+
+        [Required(ErrorMessage = "Campo Email é obrigatório.")]
+        [StringLength(255)]
+        [EmailAddress(ErrorMessage = "Formato de email inválido.")]
+        public string Email { get; set; }
+
+        public List<Divida> Dividas { get; set; } = new();
+
+        public void PrintDados()
+        {
+            Console.WriteLine("Nome: {0}", Nome);
+            Console.WriteLine("CPF: {0}", Cpf);
+            Console.WriteLine("Data Nascimento: {0}", DataNascimento.ToString("dd/MM/yyyy"));
+            Console.WriteLine("Idade: {0}", Idade);
+            Console.WriteLine("Email: {0}", Email);
+        }
+
+        public void PrintDividasAbertas()
+        {
+            Console.WriteLine("Nome: {0}", Nome);
+            Console.WriteLine("Idade: {0}", Idade);
+            foreach (Divida divida in Dividas.Where(d => !d.Pago))
+            {
+                divida.PrintDados();
+            }
+        }
+
+        public void PrintDividas()
+        {
+            Console.WriteLine("Nome: {0}", Nome);
+            Console.WriteLine("CPF: {0}", Cpf);
+            Console.WriteLine("Data Nascimento: {0}", DataNascimento.ToString("dd/MM/yyyy"));
+            Console.WriteLine("Idade: {0}", Idade);
+            Console.WriteLine("Email: {0}", Email);
+            foreach (Divida divida in Dividas)
+            {
+                divida.PrintDados();
+            }
+        }
+        
+
+    }
+}
---
+++ backend/SistemaDividaConsole/Program.cs
@@ -0,0 +1,254 @@
+﻿using SistemaDividasConsole.Models;
+using SistemaDividasConsole.Services;
+
+var service = new ClienteService();
+var dividaService = new DividaService();
+
+while (true)
+{
+    Console.WriteLine("""
+        -------- MENU --------
+        1 - Cadastrar Cliente
+        2 - Alterar Dados
+        3 - Excluir Cliente
+        4 - Listar Clientes
+        5 - Listar Clientes com dívidas em aberto
+        6 - Buscar Cliente
+        7 - Registrar Divida
+        8 - Registrar Pagamento
+        0 - Sair do Programa
+
+        Escolha uma opção: 
+        """);
+    var entrada = Console.ReadLine();
+
+    if (entrada == null || !int.TryParse(entrada, out int opcao))
+    {
+        Console.WriteLine("ERRO! Digite apenas números válidos.\n");
+        continue;
+        
+    }
+
+    if (opcao < 0 || opcao > 8) 
+    {
+        Console.WriteLine("Digite uma opção válida");
+        continue;
+    }
+
+    if (opcao == 1)
+    {
+        Console.WriteLine("Nome: ");
+        var nome = Console.ReadLine();
+
+        Console.WriteLine("CPF: ");
+        var cpf = Console.ReadLine();
+
+        Console.WriteLine("Data Nascimento: ");
+        var data_nascimento = DateTime.Parse(Console.ReadLine());
+
+        Console.WriteLine("Email: ");
+        var email = Console.ReadLine();
+
+
+        Cliente cliente = new Cliente();
+
+        cliente.Nome = nome;
+        cliente.Cpf = cpf;
+        cliente.DataNascimento = data_nascimento;
+        cliente.Email = email;
+
+        var sucesso = service.Criar(cliente, out var erros);
+        if (!sucesso)
+        {
+            foreach (var erro in erros)
+            {
+               Console.WriteLine(erro.ErrorMessage);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Cliente cadastrado com sucesso");
+        }
+
+    }
+    else if (opcao == 2)
+    {
+        Console.WriteLine("Digite o cpf do cliente: ");
+        var cpf = Console.ReadLine();
+        var cliente = service.BuscaCpf(cpf);
+
+        if (cliente == null)
+        {
+            Console.WriteLine("Cliente não encontrado");
+            continue;
+        }
+        
+        
+        Console.WriteLine("Digite o nome: ");
+        var novoNome = Console.ReadLine();
+        Console.WriteLine("Digite o email: ");
+        var novoEmail = Console.ReadLine();
+        Console.WriteLine("Digite a data de nascimento");
+        var novoNascimento = DateTime.Parse(Console.ReadLine());
+        
+
+        Cliente clienteAtualizado = new Cliente();
+
+        clienteAtualizado.Nome = novoNome;
+        clienteAtualizado.Email = novoEmail;
+        clienteAtualizado.DataNascimento = novoNascimento;
+        clienteAtualizado.Cpf = cpf;
+
+        var sucesso = service.Atualizar(cpf, clienteAtualizado, out var errosAtualizar);
+        
+        if (!sucesso)
+        {
+            foreach (var erro in errosAtualizar)
+            {
+                Console.WriteLine(erro.ErrorMessage);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Cliente atualizado com sucesso.");
+        }            
+    }
+
+    else if (opcao == 3)
+    {
+        Console.WriteLine("Digite um cpf: ");
+        var cpf = Console.ReadLine();
+        var sucesso = service.Excluir(cpf, out var errosExcluir);
+
+        if (!sucesso)
+        {
+            foreach (var erro in errosExcluir)
+            {
+                Console.WriteLine(erro.ErrorMessage);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Cliente excluido com sucesso");
+        }
+    }
+
+    else if (opcao == 4)
+    {        
+        var clientes = service.Listar();
+        if (clientes.Count == 0)
+        {
+            Console.WriteLine("Nenhum cliente cadastrado.");
+        }
+        else
+        {
+            foreach (var item in clientes)
+            {
+                Console.WriteLine("<--------------------->");
+                item.PrintDados();
+                Console.WriteLine("<--------------------->");
+            }
+        }            
+    }
+
+    else if (opcao == 5)
+    {
+        var clientes = service.ListarDividas();
+        if (clientes.Count == 0)
+        {
+            Console.WriteLine("Nenhum cliente com dívida em aberto encontrado.");
+        }
+        else
+        {
+            foreach (var item in clientes)
+            {
+                Console.WriteLine("<--------------------->");
+                item.PrintDividasAbertas();
+                Console.WriteLine("<--------------------->");
+            }
+        }
+    }
+
+    else if (opcao == 6)
+    {
+        Console.WriteLine("Digite o nome do cliente.");
+        var nome = Console.ReadLine();
+
+        var clientes = service.Buscar(nome);
+        if (clientes.Count == 0)
+        {
+            Console.WriteLine("Nenhum cliente encontrado");
+        }
+        else
+        {
+            foreach (var item in clientes)
+            {
+                item.PrintDividas();
+            }
+        }
+    }
+
+    else if (opcao == 7)
+    {
+        Console.WriteLine("Informe o cpf do cliente: ");
+        var cpf = Console.ReadLine();
+
+        var cliente = service.BuscaCpf(cpf);
+
+        if (cliente == null)
+        {
+            Console.WriteLine("Cliente não encontrado");
+        }
+        else
+        {
+            Console.WriteLine("Digite o valor da dívida: ");
+            var valor = decimal.Parse(Console.ReadLine());
+
+            Divida divida = new Divida();
+            divida.Valor = valor;
+
+            var sucesso = dividaService.Criar(divida, cliente, out var erros);
+
+            if (!sucesso)
+            {
+                foreach (var erro in erros)
+                {
+                    Console.WriteLine(erro.ErrorMessage);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Dívida registrada com sucesso.");
+            }
+        }
+    }
+
+    else if (opcao == 8)
+    {
+        Console.WriteLine("Informe o cpf do cliente: ");
+        var cpf = Console.ReadLine();
+
+        var cliente = service.BuscaCpf(cpf);
+
+        if (cliente == null)
+        {
+            Console.WriteLine("Cliente não encontrado.");
+            continue;
+        }
+        var pagamento = dividaService.Pagar(cliente);
+
+        if (pagamento)
+        {
+            Console.WriteLine("Pagamento realizado com sucesso.");
+        }
+        else
+        {
+            Console.WriteLine("Nenhuma dívida em aberto resgitrada.");
+        }
+    }
+
+    else if (opcao == 0)
+    {
+        break;
+    }
+}
